f8636398d60d82832f875ad1a5629f474ca7d8d6
Victor Kane <victorkane@gmail.com>
2021-03-31 13:01:31 -0300

    feat(import legacy post tags): for #5 legacy tags imported as post_tag taxonomy items (tags) with legacy info stored as meta

--- scripts/import-tags.php
+++ scripts/import-tags.php
@@ -3,6 +3,8 @@
  * execution
  * 
  * $ wp eval-file scripts/import-tags.php data/inv-terms.txt
+ * 
+ * test with wp-cli: wp term list post_tag
  */
 
 global $argv;
@@ -18,7 +20,9 @@ $legacy_terms = file($filename);
 // print_r($legacy_terms, false);
 
 foreach($legacy_terms as $legacy_term) {
-  print_r(json_decode($legacy_term, true));
+  $lt = json_decode($legacy_term, true);
+  echo "tag: " . $lt['tagSlug'] . "\n";
+  // print_r($lt, true);
   // Array
   // (
     // [tagSlug] => drush
@@ -38,4 +42,30 @@ foreach($legacy_terms as $legacy_term) {
         // )
   // 
   // )
+
+
+  // https://developer.wordpress.org/reference/functions/wp_insert_term/
+  $term_id = wp_insert_term(
+    $lt['tagName'], // the term 
+    'post_tag', // the taxonomy: custom, else post_tag or category
+    array(
+      'description'=> $lt['tagDescription'],
+      'slug' => $lt['tagSlug'],
+	  // not applicable in this project
+      // 'parent'=> $parent_term['term_id']  // get numeric term id
+	)
+  );
+  echo $term_id;
+  print_r($term_id, false);
+
+  if ( ! is_wp_error( $term_id ) ) {
+	echo "tag: " . $term_id['term_id'] . " / taxonomy: " . $term_id['term_taxonomy_id'] . " successfully inserted"; 
+
+	// insert meta data for this term
+	// update_term_meta( $term_id['term_id'], 'legacy_tag', array('lt' => $lt['legacyTag']));
+	update_term_meta( $term_id['term_id'], 'legacy_tag', $lt['legacyTag']);
+  } else {
+	// show error
+    print_r($term_id);     
+  }
 }
