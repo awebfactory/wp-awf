dafabd1cc50b8f5c489ea6f0b9a4a49473bedc77
Victor Kane <victorkane@gmail.com>
2021-04-01 20:19:50 -0300

    feat(import legacy posts): for #5, from from stories and blog posts, can read legacy JSON content listing

--- scripts/import-stories.php
+++ scripts/import-stories.php
@@ -2,12 +2,22 @@
 /**
  * execution
  * 
- * $ wp eval-file scripts/import-stories.php data/inv-stories.txt
+ * $ wp eval-file scripts/import-stories.php data/inv-stories.json
  */
 
 // get file path as command-line argument argument
 global $argv;
 // print_r($argv, false);
+// $legacy_stories = get_array($argv[3]);
 
-$legacy_stories = file_get_contents($argv[3]);
-print_r($legacy_stories, false);
+$legacy_stories = json_decode(file_get_contents($argv[3]), true);
+// print_r($legacy_stories, false);
+// print count($legacy_stories) . "\n";
+
+
+
+// iterate over users and upsert them
+foreach ($legacy_stories as $legacy_story) {
+	echo $legacy_story['title'] . "\n";
+
+}
