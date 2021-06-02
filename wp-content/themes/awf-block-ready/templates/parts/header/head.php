e7312f53963e423e836a927259a995500143bdaf
Victor Kane <victorkane@gmail.com>
2021-06-02 15:16:02 -0300

    chore(meta tags): add description for #7

---
+++ wp-content/themes/awf-block-ready/templates/parts/header/head.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * HTML head content.
+ *
+ * @package    Michelle
+ * @copyright  WebMan Design, Oliver Juhas
+ *
+ * @since  1.0.0
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+?>
+
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="Victor Kane. Work as praxis in word plus code plus mentor plus poet for real needs. A process engineer and system architect focusing on a lean/UX and agile approach to web application development based on best of class frameworks, communities and tools. Following a continuous improvement mentoring services team approach to web app development.">
+<link rel="profile" href="http://gmpg.org/xfn/11">
